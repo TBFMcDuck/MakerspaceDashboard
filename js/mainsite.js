a4fe8c2ce2934d1652d07d125f6a20cd13df63de
TBFMcDuck <trym.mcduck@gmail.com>
2024-08-30 22:49:27 +0200

    Varius read saving messures
    Pausing inactiv tabs, only reading from updated documents, pausing multi tabs

--- js/mainsite.js
+++ js/mainsite.js
@@ -171,7 +171,7 @@ function switchDisplay () {
         searchInput.placeholder = "Søk etter printer (Ctrl + K)"
     }
 
-    refreshTable();
+    refresh();
 }
 
 // Clean status
@@ -215,152 +215,256 @@ function cleanStatus(status, progress){
     }
 }
 
-// Refresh the table when the page loads
-window.onload = function() {
-    refreshTable();
-}
-function refreshTable() {
-    // Initialize Firestore
-    var db = firebase.firestore();
+// Refresh table
+function renderTable(querySnapshot) {
+    // Remove printergrid
+    document.getElementById("printerGrid").style.display = 'none';
+    // Show top row of table
+    document.getElementById("printerTable").style.display = 'table';
 
-    // Fetch the printerdata from Firestore (real time listner)
-    db.collection("printerdata").onSnapshot((querySnapshot) => {;
-        if (currentDisplayMode === "table") {
-            // Remove printergrid
-            document.getElementById("printerGrid").style.display = 'none';
-            // Show top row of table
-            document.getElementById("printerTable").style.display = 'table';
+    var table = document.getElementById('printerTableBody')
+    table.innerHTML = ''; // Clear the table before adding new data
+    // Display loading text
+    document.getElementById('loading').style.display = 'block';
 
-            var table = document.getElementById('printerTableBody')
-            table.innerHTML = ''; // Clear the table before adding new data
-            // Display loading text
-            document.getElementById('loading').style.display = 'block';
+    // For every printer
+    querySnapshot.forEach((doc) => {
+        var item = doc.data();
+        var row = table.insertRow(-1);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        var cell4 = row.insertCell(3);
 
-            querySnapshot.forEach((doc) => {
-                var item = doc.data();
-                var row = table.insertRow(-1);
-                var cell1 = row.insertCell(0);
-                var cell2 = row.insertCell(1);
-                var cell3 = row.insertCell(2);
-                var cell4 = row.insertCell(3);
-
-                cell1.innerHTML = item.name + " (Data fetched at:" + item.updateTime + ")";
-                cell2.innerHTML = item.type;
-                if (mobileUser){
-                    cell3.innerHTML = '<a href="' + item.address + '" target="_blank">' + item.address.substring(7, 13) + '...</a>';
-                }
-                else {
-                    cell3.innerHTML = '<a href="' + item.address + '" target="_blank">' + item.address + '</a>';
-                }
-                
-                // Cleaned status
-                var cleaned_status_and_bg_color = cleanStatus(item.status, item.progress);
-                cell4.appendChild(cleaned_status_and_bg_color[0]);
-                cell4.style.backgroundColor = cleaned_status_and_bg_color[1];
-                cell4.className = 'status-cell';
-
-                // Hide loading text
-                document.getElementById('loading').style.display = 'none';
-                // Reapply the sort
-                if (lastSortColumn !== null) {
-                    sortTable(lastSortColumn);
-                    // If your sort function doesn't automatically toggle the sort order,
-                    // you might need to apply the sort twice to get the correct order
-                    if (lastSortOrder === "desc") {
-                        console.log('Sorting again');
-                        sortTable(lastSortColumn);
-                    }
-                }
-            });
+        cell1.innerHTML = item.name + " (Fetched:" + item.updateTime + ")";
+        cell2.innerHTML = item.type;
+        if (mobileUser){
+            cell3.innerHTML = '<a href="' + item.address + '" target="_blank">' + item.address.substring(7, 13) + '...</a>';
         }
         else {
-            // Hide loading text
-            document.getElementById('loading').style.display = 'none';
-            // Hide pritnertable
-            document.getElementById("printerTable").style.display = 'none';
+            cell3.innerHTML = '<a href="' + item.address + '" target="_blank">' + item.address + '</a>';
+        }
+        
+        // Cleaned status
+        var cleaned_status_and_bg_color = cleanStatus(item.status, item.progress);
+        cell4.appendChild(cleaned_status_and_bg_color[0]);
+        cell4.style.backgroundColor = cleaned_status_and_bg_color[1];
+        cell4.className = 'status-cell';
 
-            // Show grid
-            var printergrid = document.getElementById("printerGrid");
-            printergrid.style.display = 'flex';
-            printergrid.innerHTML = '';
+        // Hide loading text
+        document.getElementById('loading').style.display = 'none';
+        // Reapply the sort
+        if (lastSortColumn !== null) {
+            sortTable(lastSortColumn);
+            // If your sort function doesn't automatically toggle the sort order,
+            // you might need to apply the sort twice to get the correct order
+            if (lastSortOrder === "desc") {
+                console.log('Sorting again');
+                sortTable(lastSortColumn);
+            }
+        }
+    });
+}
 
-            querySnapshot.forEach((doc) => {
-                var item = doc.data();
-                var newCard = document.createElement("div");
+// Refresh grid
+function renderGrid(querySnapshot) {
+    // Hide loading text
+    document.getElementById('loading').style.display = 'none';
+    // Hide pritnertable
+    document.getElementById("printerTable").style.display = 'none';
 
-                var octoprintLink = '<a href="' + item.address +  '" target="_blank"<i class="fas fa-external-link-alt" style = "color:white;"></i></a>'
-                let itemName = '<a style = "color: white; text-decoration: none;" href="' + item.address +  '" target="_blank">' + item.name + '</a>'
+    // Show grid
+    var printergrid = document.getElementById("printerGrid");
+    printergrid.style.display = 'flex';
+    printergrid.innerHTML = '';
 
-                var clean_status = cleanStatus(item.status, item.progress);
-                // Status
-                var status = document.createElement("div");
-                status.style.backgroundColor = clean_status[1];
-                status.className = 'printer-status-card';
-                if (item.status === "Printing" || item.status === "Pause") {
-                    let progressBar = new Progress({parent: status, cornerRadius: "3px", barColor: "#ff8c00", backgroundColor: "#383838", minPercent: 0.01});
-                    progressBar.setProgress(item.progress/100);
-                    progressBar.addMidElement(clean_status[0]);
+    querySnapshot.forEach((doc) => {
+        var item = doc.data();
+        var newCard = document.createElement("div");
+
+        var octoprintLink = '<a href="' + item.address +  '" target="_blank"<i class="fas fa-external-link-alt" style = "color:white;"></i></a>'
+        let itemName = '<a style = "color: white; text-decoration: none;" href="' + item.address +  '" target="_blank">' + item.name + '</a>'
+
+        var clean_status = cleanStatus(item.status, item.progress);
+        // Status
+        var status = document.createElement("div");
+        status.style.backgroundColor = clean_status[1];
+        status.className = 'printer-status-card';
+        if (item.status === "Printing" || item.status === "Pause") {
+            let progressBar = new Progress({parent: status, cornerRadius: "3px", barColor: "#ff8c00", backgroundColor: "#383838", minPercent: 0.01});
+            progressBar.setProgress(item.progress/100);
+            progressBar.addMidElement(clean_status[0]);
+        }
+        else {
+            status.appendChild(clean_status[0]);
+        }
+
+        var lastUpdated = document.createElement("p");
+        lastUpdated.innerHTML = '<p class=printer-bottom-text> Fetched: ' + item.updateTime + '</p>';
+
+        newCard.className = "printer-card";
+        newCard.innerHTML = '<h3>' + itemName + " " + octoprintLink + '</h3>' + '<h4> Model: ' + item.type + '</h4>'
+        newCard.appendChild(status);
+
+        // Timeleft
+        if (item.status === "Printing" || item.status === "Pause") {
+            let timeLeftDiv = document.createElement("div");
+            timeLeftDiv.className = "timeleftDiv";
+            
+            let printTime = item.printTime;
+            let printTimeUnit = "s";
+            if (printTime >= 60) {
+                printTime = printTime/60;
+                printTimeUnit = "min";
+                if (printTime >= 60) {
+                    printTime = printTime/60;
+                    printTimeUnit = "h";
                 }
-                else {
-                    status.appendChild(clean_status[0]);
+            }
+            let timeLeft = item.timeLeft + item.printTime;
+            let timeLeftUnit = "s";
+            if (timeLeft >= 60) {
+                timeLeft = timeLeft/60;
+                timeLeftUnit = "min";
+                if (timeLeft >= 60) {
+                    timeLeft = timeLeft/60;
+                    timeLeftUnit = "h";
                 }
+            }
 
-                var lastUpdated = document.createElement("p");
-                lastUpdated.innerHTML = '<p class=printer-bottom-text> Fetched: ' + item.updateTime + '</p>';
+            timeLeftDiv.innerHTML = '<p class="timeleftText">' + printTime.toFixed(1)+ timeLeftUnit + "/" + timeLeft.toFixed(1) + printTimeUnit + '</p>';
+            newCard.appendChild(timeLeftDiv);
+        }
 
-                newCard.className = "printer-card";
-                newCard.innerHTML = '<h3>' + itemName + " " + octoprintLink + '</h3>' + '<h4> Model: ' + item.type + '</h4>'
-                newCard.appendChild(status);
+        newCard.appendChild(lastUpdated);
 
-                // Timeleft
-                if (item.status === "Printing" || item.status === "Pause") {
-                    let timeLeftDiv = document.createElement("div");
-                    timeLeftDiv.className = "timeleftDiv";
-                    
-                    let printTime = item.printTime;
-                    let printTimeUnit = "s";
-                    if (printTime >= 60) {
-                        printTime = printTime/60;
-                        printTimeUnit = "min";
-                        if (printTime >= 60) {
-                            printTime = printTime/60;
-                            printTimeUnit = "h";
-                        }
-                    }
-                    let timeLeft = item.timeLeft + item.printTime;
-                    let timeLeftUnit = "s";
-                    if (timeLeft >= 60) {
-                        timeLeft = timeLeft/60;
-                        timeLeftUnit = "min";
-                        if (timeLeft >= 60) {
-                            timeLeft = timeLeft/60;
-                            timeLeftUnit = "h";
-                        }
-                    }
+        // Adress
+        var address = document.createElement("p");
+        address.innerHTML = '<p class="adressBottomText">' + item.address + '<p>';
+        
+        newCard.appendChild(address);
 
-                    timeLeftDiv.innerHTML = '<p class="timeleftText">' + printTime.toFixed(1)+ timeLeftUnit + "/" + timeLeft.toFixed(1) + printTimeUnit + '</p>';
-                    newCard.appendChild(timeLeftDiv);
-                }
+        printergrid.appendChild(newCard);
+    });
+}
 
-                newCard.appendChild(lastUpdated);
+// Create a BroadcastChannel
+const channel = new BroadcastChannel('tab-status');
 
-                // Adress
-                var address = document.createElement("p");
-                address.innerHTML = '<p class="adressBottomText">' + item.address + '<p>';
-                
-                newCard.appendChild(address);
+// Variable to track if this tab should update
+let shouldUpdate = true;
 
-                printergrid.appendChild(newCard);
-            });
+// Variable to not read unupdated pages
+let lastUpdateTime = null;
+
+// Variable to store the Firestore unsubscribe function
+let unsubscribe = null;
+
+// Updating text
+let updatingText = document.getElementById("updatedAutoText");
+
+// Enable Firestore offline persistence, to decrease reads probatly
+firebase.firestore().enablePersistence()
+    .catch(function(err) {
+        if (err.code == 'failed-precondition') {
+            console.error('Multiple tabs open, persistence can only be enabled in one tab at a a time.');
+        } else if (err.code == 'unimplemented') {
+            console.error('The current browser does not support all of the features required to enable persistence');
+        }
+    });
+
+// Listen for messages from other tabs
+channel.onmessage = (event) => {
+    if (event.data === "active") {
+        shouldUpdate = false;
+        if (unsubscribe) {
+            unsubscribe();
+            unsubscribe = null;
+            updatingText.innerHTML = 'Updates paused because other tab is active. <i class="fas fa-pause-circle"></i>';
+            document.title = '(PAUSED) Makerspace Dashboard';
+        }
+    }
+};
+
+// Send a message when this tab becomes active
+window.addEventListener('focus', () => {
+    shouldUpdate = true;
+    channel.postMessage('active');
+    subscribeToFirestore();
+    updatingText.innerHTML = 'Updates automatically <i class="fas fa-check"></i>';
+    document.title = 'Makerspace Dashboard';
+});
+
+// Handle when the tab becomes inactive
+window.addEventListener('blur', () => {
+    shouldUpdate = false;
+    if (unsubscribe) {
+        unsubscribe();
+        unsubscribe = null;
+        updatingText.innerHTML = 'Updates paused because other tab is active. <i class="fas fa-pause-circle"></i>';
+        document.title = '(PAUSED) Makerspace Dashboard';
+    }
+});
+
+// Initialize Firestore
+var db = firebase.firestore();
+
+// Refresh the table when the page loads
+window.onload = function() {
+    refresh();
+}
+
+function refresh() {
+    subscribeToFirestore();
+}
+
+function subscribeToFirestore() {
+    if (unsubscribe) {
+        unsubscribe();
+    }
+
+    let query = db.collection("printerdata");
+    if (lastUpdateTime) {
+        query = query.where("updateTime", ">", lastUpdateTime);
+    }
+
+    // Fetch the printerdata from Firestore (real time listener)
+    unsubscribe = query.onSnapshot((querySnapshot) => {
+        // Check if this tab should update
+        if (!shouldUpdate) return;
+
+        if (currentDisplayMode === "table") {
+            renderTable(querySnapshot);
+        } else {
+            renderGrid(querySnapshot);
         }
 
         // Reapply search
         searchFunction();
 
+        // Update the lastUpdateTime to the latest document's updateTime
+        if (!querySnapshot.empty) {
+            let maxUpdateTime = querySnapshot.docs[0].data().updateTime;
+            querySnapshot.forEach(doc => {
+                const updateTime = doc.data().updateTime;
+                if (updateTime > maxUpdateTime) {
+                    maxUpdateTime = updateTime;
+                }
+            });
+            lastUpdateTime = maxUpdateTime;
+        }
+
     }, error => {
+        if (error.code === 'resource-exhausted') {
+            document.getElementById('loading').style.display = 'block';
+            document.getElementById('loading').innerHTML = 'Quota exceeded. Please try again tomorrow.';
+            updatingText.innerHTML = 'Updating failed due to quota limit <i class="fas fa-exclamation-circle"></i>';
+        } else {
+            document.getElementById('loading').style.display = 'block';
+            document.getElementById('loading').innerHTML = 'Failed to fetch from Firestore.';
+            updatingText.innerHTML = 'Updating failed <i class="fas fa-exclamation-circle"></i>';
+        }
         console.error('Error:', error);
-        // Hide loading text
-        document.getElementById('loading').style.display = 'none';
     });
 }
 
